029e1cc3a7e9846485f69ff3cf44fec9797ffff2
SusejR <rodriguezbsusej@gmail.com>
2020-02-20 12:32:53 -0300

    agregando status #2

--- cli.js
+++ cli.js
@@ -1,10 +1,13 @@
 
 const routePath = require('path');
 const myFile = require('./index.js');
+const options = {};
 
 let path = process.argv[2];
 path = routePath.resolve(path);
 path = routePath.normalize(path);
 
 myFile(path)
-.then(path => console.log(path));
+    .then(path => {
+     //   console.log(path)
+    }).catch(error => { console.log(error)});
--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-// module.exports = () => {
-//   // ...
-// };
+
 const fs = require('fs');
-const file = (path => {
-    let promise = new Promise ((resolve, reject) => {
+const marked = require('marked');
+const fetch = require('node-fetch');
+const chalk = require('chalk');
+
+const filteredLinks = (links) => {
+    return links.filter((link) => {
+        const prefix = link.href.substring(0, 4);
+        if (prefix == 'http') {
+            return true;
+        } else {
+            return false;
+        }
+    })
+}
+const file = (path, options) => {
+    return new Promise((resolve, reject) => {
         fs.readFile(path, 'utf8', (error, data) => {
-            if(error){
+            if (error) {
                 reject(new Error('No existe archivo'));
+            }
+            let links = [];
+            const renderer = new marked.Renderer();
+
+            renderer.link = (href, title, text) => {
+            
+                links.push({
+                    href: href,
+                    text: text,
+                    file: path,
+                });
             };
-            resolve(data);
+            marked(data, { renderer: renderer });
+            links = filteredLinks(links);
+            links = status(links);
+
+            resolve(links);
         });
     });
-return promise
-});
+};
+
+const status = (links) => {
+    links.filter(link => {
+        fetch(link.href)
+            .then(response => {
+                if (response.status == 200) {
+                    console.log(chalk.yellow(link.text) +' ' + chalk.blue(link.href) + ' ' + chalk.green('status ' + response.status));
+                } else {
+                    console.log(chalk.yellow(link.text) + chalk.blue(link.href) + chalk.red(response.status));
+                }
+            })
+            .catch(error => {
+                console.log(chalk.red('Este link no funciona ' + link.href));
+            })
+
+    });
+};
+
+
+
 module.exports = file;
+
